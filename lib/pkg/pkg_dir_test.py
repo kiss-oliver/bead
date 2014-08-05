5e9766f2c22d7772387196c05828cd12ed7bb5dd
Krisztián Fekete <fekete.krisztyan@gmail.com>
2014-08-05 16:51:38 +0200

    pkg_dir test

---
+++ lib/pkg/pkg_dir_test.py
@@ -0,0 +1,32 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import unicode_literals
+from __future__ import print_function
+
+from ..test import TestCase
+from . import pkg_dir
+
+import fixtures
+
+from ..path import Path
+
+
+class Test(TestCase):
+
+    def test(self):
+        self.given_an_empty_directory()
+        self.when_initialized()
+        self.then_directory_is_a_valid_pkg_dir()
+
+    # implementation
+
+    __pkg_dir = None
+
+    def given_an_empty_directory(self):
+        self.__pkg_dir = self.useFixture(fixtures.TempDir()).path
+
+    def when_initialized(self):
+        pkg_dir.create(self.__pkg_dir)
+
+    def then_directory_is_a_valid_pkg_dir(self):
+        self.assertTrue(pkg_dir.is_valid(Path(self.__pkg_dir)))
