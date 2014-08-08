f96ee95e1a1148393c37a6d0b2965e32f859b16a
Krisztián Fekete <fekete.krisztyan@gmail.com>
2014-08-08 18:08:59 +0200

    timestamps

---
+++ lib/timestamp.py
@@ -0,0 +1,38 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import unicode_literals
+from __future__ import print_function
+
+from datetime import tzinfo, timedelta, datetime
+
+ZERO = timedelta(0)
+HOUR = timedelta(hours=1)
+
+
+# source: https://docs.python.org/2/library/datetime.html#tzinfo-objects
+class UTC(tzinfo):
+    """UTC"""
+
+    def utcoffset(self, dt):
+        return ZERO
+
+    def tzname(self, dt):
+        return "UTC"
+
+    def dst(self, dt):
+        return ZERO
+
+
+TIMESTAMP_FORMAT = 'UTC_%Y%m%d_%H%M%S_%f'
+
+
+def timestamp():
+    return datetime.now(UTC()).strftime(TIMESTAMP_FORMAT)
+
+
+def time_from_timestamp(date_string):
+    return (
+        datetime
+        .strptime(date_string, TIMESTAMP_FORMAT)
+        .replace(tzinfo=UTC())
+    )
