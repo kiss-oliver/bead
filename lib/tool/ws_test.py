2b284b287fc3ad55498fca45069e3002490daada
Krisztián Fekete <fekete.krisztyan@gmail.com>
2014-08-28 19:51:48 +0200

    add ws_test for untested code to show up in coverage

---
+++ lib/tool/ws_test.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import unicode_literals
+from __future__ import print_function
+
+from . import ws as m
+
+assert m  # silence pyflakes
