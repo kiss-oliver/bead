48d2780ee3ff0d3b17e4875af3032a94e9029543
Fekete Krisztián <fekete.krisztyan@gmail.com>
2014-09-10 12:12:20 +0200

    new functions for path module

--- lib/path.py
+++ lib/path.py
@@ -3,7 +3,9 @@ from __future__ import division
 from __future__ import unicode_literals
 from __future__ import print_function
 
+import codecs
 import os
+import stat
 import contextlib
 import shutil
 import tempfile
@@ -32,14 +34,14 @@ def write_file(path, content):
     if isinstance(content, bytes):
         f = open(path, 'wb')
     else:
-        f = open(path, 'wt', encoding='utf-8')
+        f = codecs.open(path, 'wb', encoding='utf-8')
 
     with f:
         f.write(content)
 
 
 @contextlib.contextmanager
-def temp_dir(dir):
+def temp_dir(dir='.'):
     ensure_directory(dir)
 
     temp_dir = tempfile.mkdtemp(dir=dir)
@@ -47,3 +49,21 @@ def temp_dir(dir):
         yield Path(temp_dir)
     finally:
         shutil.rmtree(temp_dir, ignore_errors=True)
+
+
+def make_readonly(path):
+    mode = os.stat(path)[stat.ST_MODE]
+    os.chmod(path, mode & ~stat.S_IWRITE)
+
+
+def make_writable(path):
+    mode = os.stat(path)[stat.ST_MODE]
+    os.chmod(path, mode | stat.S_IWRITE)
+
+
+def all_subpaths(dir):
+    for root, dirs, files in os.walk(dir):
+        root = Path(root)
+        yield root
+        for file in files:
+            yield root / file
---
+++ lib/path_test.py
@@ -0,0 +1,124 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import unicode_literals
+from __future__ import print_function
+
+from .test import TestCase
+from . import path as m
+
+import os
+
+
+class TestPath(TestCase):
+
+    def test_div(self):
+        result = m.Path('a') / 'b'
+        self.assertEquals(
+            ('a', os.path.sep, 'b'),
+            result.partition(os.path.sep)
+        )
+
+
+class Test_make_readonly(TestCase):
+
+    def test_file(self):
+        self.given_a_file()
+        self.when_made_readonly()
+        self.then_file_can_not_be_written()
+
+    def test_directory(self):
+        self.given_a_directory()
+        self.when_made_readonly()
+        self.then_can_not_creat_file_under_directory()
+
+    # implementation
+
+    __path = None
+
+    def given_a_file(self):
+        self.__path = self.new_temp_dir() / 'file'
+        with open(self.__path, 'wb'):
+            pass
+
+    def given_a_directory(self):
+        self.__path = self.new_temp_dir()
+
+    def when_made_readonly(self):
+        m.make_readonly(self.__path)
+
+    def then_file_can_not_be_written(self):
+        self.assertRaises(IOError, open, self.__path, 'wb')
+
+    def then_can_not_creat_file_under_directory(self):
+        self.assertRaises(IOError, open, self.__path / 'file', 'wb')
+
+
+class Test_make_writable(TestCase):
+
+    def test_file(self):
+        self.given_a_read_only_file()
+        self.when_made_writable()
+        self.then_file_can_be_written()
+
+    def test_directory(self):
+        self.given_a_read_only_directory()
+        self.when_made_writable()
+        self.then_file_can_be_created_under_directory()
+
+    # implementation
+
+    __path = None
+
+    def given_a_read_only_file(self):
+        self.__path = self.new_temp_dir() / 'file'
+        with open(self.__path, 'wb'):
+            pass
+        m.make_readonly(self.__path)
+
+    def given_a_read_only_directory(self):
+        self.__path = self.new_temp_dir()
+        m.make_readonly(self.__path)
+
+    def when_made_writable(self):
+        m.make_writable(self.__path)
+
+    def then_file_can_be_written(self):
+        with open(self.__path, 'ab') as f:
+            f.write(b'little something')
+
+    def then_file_can_be_created_under_directory(self):
+        with open(self.__path / 'file', 'wb') as f:
+            f.write(b'little something')
+
+
+class Test_all_subpaths(TestCase):
+
+    def test(self):
+        self.given_some_directory_structure_with_files()
+        self.when_all_paths_are_collected()
+        self.then_all_paths_are_found()
+
+    # implementation
+
+    __root = None
+    DIRS = ('a', 'b', 'c', 'c/d')
+    FILES = ('a/f', 'c/d/f1', 'c/d/f2')
+
+    def given_some_directory_structure_with_files(self):
+        self.__root = root = self.new_temp_dir()
+        for dir in self.DIRS:
+            os.makedirs(root / dir)
+        for f in self.FILES:
+            m.write_file(root / f, '')
+
+    def when_all_paths_are_collected(self):
+        self.__paths = set(m.all_subpaths(self.__root))
+
+    def then_all_paths_are_found(self):
+        root = self.__root
+        self.assertEquals(
+            {root}
+            | set(root / d for d in self.DIRS)
+            | set(root / f for f in self.FILES),
+            self.__paths
+        )
