47f27f34b9bc6ebfba00af8f43c8884b38b9a7af
Fekete Krisztián <fekete.krisztyan@gmail.com>
2015-02-02 14:26:21 +0100

    protect HOME directory from integration test
    Command new is about to improve so that it uses a user specific
    persistent name database, which means tests would modify the user's
    environment unless protected, so test is modified to create a fake HOME.

--- lib/tests/integration_tests.py
+++ lib/tests/integration_tests.py
@@ -1,20 +1,25 @@
-from nose import tools
 from scripttest import TestFileEnvironment
 
+import fixtures
+from testtools import TestCase
 
-def test_package_operations():
-    env = TestFileEnvironment()
-    result = env.run('ws', 'new', 'something')
 
-    tools.assert_in('something', result.files_created)
+class Test_command_line(TestCase):
 
-    mounts_list = env.run('ws', 'mounts', cwd=env.cwd + '/something')
-    tools.assert_in('no defined inputs', mounts_list.stdout)
+    def test_package_operations(self):
+        self.useFixture(fixtures.TempHomeDir())
+        env = TestFileEnvironment()
+        result = env.run('ws', 'new', 'something')
 
-    pack_result = env.run('ws', 'pack', cwd=env.cwd + '/something')
+        self.assertIn('something', result.files_created)
 
-    package, = pack_result.files_created.keys()
+        mounts_list = env.run('ws', 'mounts', cwd=env.cwd + '/something')
+        self.assertIn('no defined inputs', mounts_list.stdout)
 
-    result = env.run('ws', 'develop', 'something-develop', package)
+        pack_result = env.run('ws', 'pack', cwd=env.cwd + '/something')
 
-    tools.assert_in('something-develop', result.files_created)
+        package, = pack_result.files_created.keys()
+
+        result = env.run('ws', 'develop', 'something-develop', package)
+
+        self.assertIn('something-develop', result.files_created)
