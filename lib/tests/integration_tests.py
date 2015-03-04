13abfda1ac1b087369681c457cbe1e499b0aa511
Fekete Krisztián <fekete.krisztyan@gmail.com>
2015-03-04 13:10:26 +0100

    fix script tests - failing commands are catched

--- lib/tests/integration_tests.py
+++ lib/tests/integration_tests.py
@@ -9,17 +9,20 @@ class Test_command_line(TestCase):
     def test_package_operations(self):
         self.useFixture(fixtures.TempHomeDir())
         env = TestFileEnvironment()
-        result = env.run('ws', 'new', 'something')
 
-        self.assertIn('something', result.files_created)
+        new_result = env.run('ws', 'new', 'something')
+        self.assertEqual(0, new_result.returncode)
+        self.assertIn('something', new_result.files_created)
 
-        mounts_list = env.run('ws', 'status', cwd=env.cwd + '/something')
-        self.assertIn('no defined inputs', mounts_list.stdout)
+        status_result = env.run('ws', 'status', cwd=env.cwd + '/something')
+        self.assertIn('no defined inputs', status_result.stdout)
+        self.assertEqual(0, status_result.returncode)
 
         pack_result = env.run('ws', 'pack', cwd=env.cwd + '/something')
+        self.assertEqual(0, pack_result.returncode)
 
         package, = pack_result.files_created.keys()
 
-        result = env.run('ws', 'develop', 'something-develop', package)
-
-        self.assertIn('something-develop', result.files_created)
+        develop_result = env.run('ws', 'develop', 'something-develop', package)
+        self.assertIn('something-develop', develop_result.files_created)
+        self.assertEqual(0, develop_result.returncode)
