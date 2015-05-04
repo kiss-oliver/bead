ad2711d8734b9fe1956554e51c0033479b9e54eb
Fekete Krisztián <fekete.krisztyan@gmail.com>
2015-05-04 15:08:49 +0200

    connecting to database (not called yet)

--- lib/db.py
+++ lib/db.py
@@ -3,8 +3,24 @@ from __future__ import division
 from __future__ import unicode_literals
 from __future__ import print_function
 
+import os
+
 import omlite
 from .translations import Peer, Translation
+
+
+def connect(db_path):
+    existing = os.path.exists(db_path)
+    omlite.db.connect(db_path)
+
+    try:
+        if not existing:
+            initialize_new_db()
+    except:
+        omlite.db.connect(':memory:')
+        # remove newly created, but partially initialized database
+        os.remove(db_path)
+        raise
 
 
 def initialize_new_db():
@@ -19,7 +35,8 @@ def initialize_new_db():
 
 def _create_self():
     # create self as peer with special empty name
-    self = Peer(name='')
-    omlite.create(self)
-    self.value = self.id
-    omlite.save(self)
+    with omlite.db.transaction():
+        self = Peer(name='')
+        omlite.create(self)
+        self.value = self.id
+        omlite.save(self)
