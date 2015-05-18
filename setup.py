4ef03467406bf608f0a3f3bcc53edfbd9095cc74
Fekete Krisztián <fekete.krisztyan@gmail.com>
2015-05-18 14:26:02 +0200

    specified license, omlite git repository in setup.py
    This makes information in setup.cfg redundant!

--- setup.py
+++ setup.py
@@ -1,4 +1,43 @@
 #!/usr/bin/env python
-from setuptools import setup
+# coding: utf-8
+from setuptools import setup, find_packages
 
-setup(setup_requires=['pbr'], pbr=True)
+# PBR does not work with VCS requirements, yet:
+# https://bugs.launchpad.net/pbr/+bug/1373623
+# setup(setup_requires=['pbr'], pbr=True)
+
+
+def content_of_file(filename):
+    with open(filename) as f:
+        return f.read()
+
+setup(
+    name='lib',
+    version='0.0.1',
+    description='Data package tool',
+    long_description=content_of_file('README.md'),
+    author='Krisztián Fekete',
+    author_email='krisztian.fekete@gmail.com',
+    url='https://github.com/krisztianfekete/lib',
+    license=content_of_file('LICENSE'),
+    packages=find_packages(exclude=('tests', 'docs')),
+
+    install_requires=[
+        'appdirs==1.4.0',
+        'argh==0.26.1',
+        'omlite',
+    ],
+    dependency_links=[
+        'git+https://github.com/krisztianfekete/omlite#egg=omlite',
+    ],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'License :: Public Domain',
+    ],
+    entry_points={
+        'console_scripts': [
+            'ws = lib.tool.ws:main'
+        ]
+    }
+)
