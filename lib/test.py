9d792278459fc3a5c3ead61c9e0afde04e985df8
Fekete Krisztián <fekete.krisztyan@gmail.com>
2015-06-08 11:54:35 +0200

    testing: @xfail - expected failure for testtools

--- lib/test.py
+++ lib/test.py
@@ -3,6 +3,7 @@ from __future__ import division
 from __future__ import unicode_literals
 from __future__ import print_function
 
+import functools
 import testtools
 import fixtures
 
@@ -10,6 +11,27 @@ from . import tech
 import os
 import tempfile
 import arglinker
+
+
+def xfail(test, why=""):
+    """testtools' version of unittest's expectedFailure decorator
+
+    The one from unittest does not work with testtools.TestCase!
+    """
+    @functools.wraps(test)
+    def expect_to_fail(self):
+        def errors_to_failure(f):
+            # convert all exceptions including SystemExit
+            # and other heavy ones to failure
+            try:
+                f(self)
+            except BaseException as e:
+                self.fail(e)
+        self.expectFailure(
+            why,
+            errors_to_failure, arglinker.func_with_fixture_resolver(test)
+        )
+    return expect_to_fail
 
 
 class TempDir(fixtures.Fixture):
