5443d7b925f9f63a0159a93f80ec8aeea64eba9e
Fekete Krisztián <fekete.krisztyan@gmail.com>
2015-06-11 04:05:51 +0200

    repositories table

--- lib/repos.py
+++ lib/repos.py
@@ -6,24 +6,43 @@ from __future__ import print_function
 
 import os
 
+from omlite import storable_pk_netaddrtime_uuid1 as storable
+from omlite import sql_constraint
+from omlite import Field
+import omlite
+
 from .pkg.archive import Archive
 from .import tech
 Path = tech.fs.Path
 
 
+TEXT_FIELD = Field('VARCHAR NOT NULL')
+UUID_FIELD = Field('VARCHAR NOT NULL')
+
+
+@storable
 class Repository(object):
+
+    # id = UUID_FIELD
+    name = TEXT_FIELD
+    location = TEXT_FIELD
+
+    def __init__(self, name=None, location=None):
+        self.location = location
+        self.name = name
+        self._impl = UserManagedDirectory()
 
     def find_package(self, uuid, version=None):
         # -> [Package]
-        pass
+        return self._impl.find_package(self, uuid, version)
 
     def find_newest(self, uuid):
         # -> Package
-        pass
+        return self._impl.find_newest(self, uuid)
 
     def store(self, workspace, timestamp):
         # -> Package
-        pass
+        return self._impl.store(self, workspace, timestamp)
 
 
 class UserManagedDirectory(Repository):
@@ -44,3 +63,5 @@ class UserManagedDirectory(Repository):
                         return package
             except:
                 pass
+
+# FIXME: remove workspace.flat_repo
