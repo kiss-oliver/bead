ba69f684dc612fbbd31972dab544de2d1b1e9847
Fekete Krisztián <fekete.krisztyan@gmail.com>
2015-08-06 15:00:32 +0200

    build.py removes the previous build directory before building
    + unix tool is made executable
    + other minor changes

--- build.py
+++ build.py
@@ -4,9 +4,10 @@ from __future__ import unicode_literals
 from __future__ import print_function
 
 import os
+import stat
 from subprocess import check_call
 from glob import glob
-from shutil import rmtree
+import shutil
 from zipfile import ZipFile, ZIP_DEFLATED
 import contextlib
 
@@ -14,7 +15,10 @@ import contextlib
 BUILD = 'build'
 PKGS = BUILD + '/pkgs'
 SRC = BUILD + '/src'
-
+CHECKOUT = BUILD + '/checkout'
+TOOL_PYZ = BUILD + '/ws.pyz'
+UNIX_TOOL = BUILD + '/ws'
+WIN_TOOL = BUILD + '/ws.cmd'
 
 def mkdir(dir):
     if not os.path.isdir(dir):
@@ -23,6 +27,15 @@ def mkdir(dir):
 
 def pip_install(*args):
     return check_call(('pip', 'install') + args)
+
+
+def rmtree(dir):
+    shutil.rmtree(dir, ignore_errors=True)
+
+
+def make_executable(file):
+    st = os.stat(file)
+    os.chmod(file, st.st_mode | stat.S_IEXEC | stat.S_IXGRP | stat.S_IXOTH)
 
 
 @contextlib.contextmanager
@@ -45,10 +58,15 @@ def further_output(msg):
     return notification(msg, long_output=True)
 
 progress = notification
+rmtree(BUILD)
 
 with further_output('Downloading dependencies'):
     mkdir(PKGS)
-    pip_install('--no-use-wheel', '--download', PKGS, '-r', 'requirements.txt')
+    pip_install(
+        '--no-use-wheel', '--download', PKGS,
+        '--src', CHECKOUT, '--exists-action', 'w',
+        '-r', 'requirements.txt')
+    rmtree(CHECKOUT)
 
 with further_output('Unpacking packages'):
     mkdir(SRC)
@@ -60,7 +78,7 @@ for dir in glob(SRC + '/*.egg-info'):
     rmtree(dir)
 
 with progress('Creating .pyz zip archive from the sources'):
-    with ZipFile(BUILD + '/ws.pyz', mode='w', compression=ZIP_DEFLATED) as zip:
+    with ZipFile(TOOL_PYZ, mode='w', compression=ZIP_DEFLATED) as zip:
         # add the entry point
         zip.write('__main__.py')
         # add python sources
@@ -76,13 +94,14 @@ with progress('Creating .pyz zip archive from the sources'):
 def make_tool(tool_file_name, runner):
     with open(tool_file_name, 'wb') as f:
         f.write(runner)
-        with open(BUILD + '/ws.pyz', 'rb') as pyz:
+        with open(TOOL_PYZ, 'rb') as pyz:
             f.write(pyz.read())
 
 with progress('Creating unix tool'):
     UNIX_RUNNER = b'#!/usr/bin/env python\n'
 
-    make_tool(BUILD + '/ws', UNIX_RUNNER)
+    make_tool(UNIX_TOOL, UNIX_RUNNER)
+    make_executable(UNIX_TOOL)
 
 with progress('Creating windows tool'):
     WINDOWS_RUNNER = b'\r\n'.join((
@@ -91,6 +110,6 @@ with progress('Creating windows tool'):
         b'exit /b %errorlevel%',
         b''))
 
-    make_tool(BUILD + '/ws.cmd', WINDOWS_RUNNER)
+    make_tool(WIN_TOOL, WINDOWS_RUNNER)
 
 print('Done.')
