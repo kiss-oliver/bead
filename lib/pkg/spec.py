84ac0778e17a0e4c459f4346e76eb9042a808e5f
Fekete Krisztián <fekete.krisztyan@gmail.com>
2016-03-08 18:06:08 +0100

    (RED) spec: refinement + beginning of cli

--- lib/pkg/spec.py
+++ lib/pkg/spec.py
@@ -12,6 +12,23 @@ PackageSpec = namedtuple('PackageSpec', 'repo name version offset')
 ALL_REPOSITORIES = object()
 
 
+'''
+-r, --repo, --repository
+
+
+package_filters
+-o, --older, --older-than
+-n, --newer, --newer-than
+-d, --date
+-N, --next
+-P, --prev, --previous
+--timedelta
+
+match reducers
+--newest (default)
+-O, --oldest
+'''
+
 _parse = re.compile(
     r'''
     ^
@@ -54,29 +71,39 @@ def parse(string):
     raise ValueError('Not a valid package specification', string)
 
 
+class PackageQuery:
+
+    def __init__(self, repositories, spec, first_matches=False):
+        self.repositories = repositories
+        self.spec = spec
+        self.first_matches = first_matches
+
+    def get_packages(self):
+        packages = []
+        for repo in self.repositories:
+            for package in self.query_repo(repo):
+                if self.matching_package(package):
+                    packages.append(package)
+                    packages = self.fold_packages(packages)
+                    if self.first_matches and packages:
+                        return packages
+        return packages
+
 
 class PackageSpec:
 
     def __init__(self):
-        self.package_queries = []
+        self.repo_queries = []
         self.package_filters = []
-        self.match_filters = []
+        self.match_reducers = []
 
-    # Select
-    # TODO: PackageSpec.allowed_repo
-    # def allowed_repo(self, repo):
-    #     '''
-    #         Filter repositories before looking at packages
-    #     '''
-    #     return True
-
-    def get_packages(self, repo):
-        packages = []
-        for package in self.query_repo(repo):
-            if self.matching_package(package):
-                packages.append(package)
-                packages = self.fold_packages(packages)
-        return packages
+    def query_repo(self, repo):
+        '''
+            Retrieve match candidates from a repository
+        '''
+        for query in self.repo_queries:
+            for package in query(repo):
+                yield package
 
     def matching_package(self, package):
         '''
@@ -93,34 +120,28 @@ class PackageSpec:
 
             e.g. keep only the latest, or the closest one
         '''
-        for match_reducer in self.match_filters:
+        for match_reducer in self.match_reducers:
             packages = match_reducer(packages)
         return packages
 
     # Construct
-    def add_package_query(self, pkg_query):
+    def add_repo_query(self, pkg_query):
         '''
             Use :pkg_query: for querying repos for package candidates.
         '''
-        self.package_queries.append(pkg_query)
+        self.repo_queries.append(pkg_query)
 
     def add_package_filter(self, pkg_filter):
         '''
             Restrict packages to those matching :pkg_filter:
         '''
-        self.pass_filter.append(pkg_filter)
+        self.package_filters.append(pkg_filter)
 
-    def add_match_filter(self, match_reducer):
+    def add_match_reducer(self, match_reducer):
         '''
             Reduce matches - keep only the best one[s]
         '''
-        self.match_filters.append(match_reducer)
-
-    # internals
-    def query_repo(self, repo):
-        for query in self.package_queries:
-            for package in query(repo):
-                yield package
+        self.match_reducers.append(match_reducer)
 
 
 # repo queries
@@ -168,3 +189,46 @@ def newest(packages):
 
 def oldest(packages):
     return sorted(packages, key=lambda pkg: pkg.timestamp)[:1]
+
+
+
+
+
+# FIXME: find proper place for spec.package_spec_kwargs, parse_package_spec_kwargs
+
+from argh import ArghParser, arg, named
+
+def package_spec_kwargs(func):
+    for modifier in [
+        arg('-o', '--older', '--older-than', dest='older_than'),
+        arg('-n', '--newer', '--newer-than', dest='newer_than'),
+        arg('-d', '--date', dest='date'),
+    ]:
+        func = modifier(func)
+    return func
+
+
+def parse_package_spec_kwargs(kwargs):
+    arg_to_filter = {
+        'older_than': older_than,
+        'newer_than': newer_than,
+        'date': timestamp_prefix,
+    }
+    spec = PackageSpec()
+    for attr in arg_to_filter:
+        spec.add_package_filter(arg_to_filter[attr](kwargs[attr]))
+    return spec
+
+
+if __name__ == '__main__':
+    @named('get')
+    @package_spec_kwargs
+    def cmd(other, **kwargs):
+        spec = parse_package_spec_kwargs(kwargs)
+        print(spec.package_filters)
+        print(other, kwargs)
+
+    p = ArghParser()
+    # p.set_default_command(cmd)
+    p.add_commands([cmd])
+    p.dispatch()
