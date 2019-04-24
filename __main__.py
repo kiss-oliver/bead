8c1e469cba560cf5102edba4d0872b16f659c566
Fekete Krisztián <e3krisztian@gmail.com>
2019-04-24 14:25:21 +0200

    check Python version on startup, refuse to run if < 3.6

--- __main__.py
+++ __main__.py
@@ -1,3 +1,6 @@
-import bead_cli.main
+import sys
+assert sys.version_info >= (3, 6), "Python version 3.6 or newer is required"
 
-bead_cli.main.main()
+if __name__ == "__main__":
+    import bead_cli.main
+    bead_cli.main.main()
