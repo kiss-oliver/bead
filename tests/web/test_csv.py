52c0e0b52e41260294f6076678f4dd28f2e3c493
Fekete Krisztián <e3krisztian@gmail.com>
2019-10-19 17:13:48 +0200

    delete obsolete tests, move tests to new place

---
+++ tests/web/test_csv.py
@@ -0,0 +1,79 @@
+import csv
+import io
+from bead_cli.web.csv import read_beads, write_beads
+
+
+INPUT_CSV = """\
+owner,name,kind,content_id,freeze_time
+id_ood1,root,kind_root_1,id_root1_ood,20180321T191922693711+0100
+
+id_ood2,ood1,kind_ood1,id_ood1,20190321T191922693711+0100
+id_ood2,root2,kind_root_2,id_root2_utd,20190321T191922693711+0100
+
+id_ood3,ood2,kind_ood2,id_ood2,20190321T191922693711+0100
+id_ood3,phantom,kind_ood2,id_phantom,20140321T191922693711+0100
+
+"""
+
+BEAD_CSV = """\
+box,name,kind,content_id,freeze_time
+
+box,ood2,kind_ood2,id_ood2,20190321T191922693711+0100
+
+box,ood1,kind_ood1,id_ood1,20190321T191922693711+0100
+
+,root2,kind_root_2,id_root2_utd,20190321T191922693711+0100
+
+,root1,kind_root_1,id_root1_utd,20190321T191922693711+0100
+,root1,kind_root_1,id_root1_ood,20180321T191922693711+0100
+
+,ood3,kind_ood3,id_ood3,20190321T191922693711+0100
+"""
+
+INPUT_MAPS_CSV = """\
+box,name,content_id,input,bead_name
+
+box,ood2,id_ood2,root2,root2
+box,ood2,id_ood2,ood1,ood1
+
+box,ood1,id_ood1,root,root1
+
+,ood3,id_ood3,phantom,real_name_of_phantom
+"""
+
+
+def test_beads():
+    return read_beads(
+        io.StringIO(BEAD_CSV),
+        io.StringIO(INPUT_CSV),
+        io.StringIO(INPUT_MAPS_CSV))
+
+
+class Test_bead_csv_io(TestCase):
+    def test_written_data_is_unchanged(self):
+        beads = test_beads()
+        beads_csv_stream = io.StringIO()
+        inputs_csv_stream = io.StringIO()
+        input_maps_csv_stream = io.StringIO()
+        write_beads(beads, beads_csv_stream, inputs_csv_stream, input_maps_csv_stream)
+
+        def read_sorted(text_csv, fields):
+            def sort_key(record):
+                return [record[field] for field in fields]
+            return sorted(csv.DictReader(io.StringIO(text_csv)), key=sort_key)
+
+        # written beads remain the same
+        sort_fields = ['content_id']
+        self.assertEqual(
+            read_sorted(BEAD_CSV, sort_fields),
+            read_sorted(beads_csv_stream.getvalue(), sort_fields))
+        # written inputs remain the same
+        sort_fields = ['owner', 'content_id']
+        self.assertEqual(
+            read_sorted(INPUT_CSV, sort_fields),
+            read_sorted(inputs_csv_stream.getvalue(), sort_fields))
+        # written input_maps remain the same
+        sort_fields = ['box', 'name', 'content_id', 'input']
+        self.assertEqual(
+            read_sorted(INPUT_MAPS_CSV, sort_fields),
+            read_sorted(input_maps_csv_stream.getvalue(), sort_fields))
