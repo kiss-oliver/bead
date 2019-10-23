8cd8f0928518859ee08302e35218ffa3319a5986
Fekete Krisztián <e3krisztian@gmail.com>
2019-10-23 14:58:21 +0200

    refactor MetaBead to use attr.s

--- bead_cli/web/metabead.py
+++ bead_cli/web/metabead.py
@@ -1,31 +1,39 @@
-from typing import List, Iterable
+from typing import Dict, List, Optional
+
+import attr
 
 from bead.meta import InputSpec
 from bead.tech.timestamp import time_from_timestamp
 from .bead_state import BeadState
 
 
+def none_to_empty_dict(v):
+    """attr converter"""
+    if v is None:
+        return {}
+    return v
+
+
+@attr.s
 class MetaBead:
     """
     A bead.Bead look-alike when looking only at the metadata.
 
     Also has metadata for coloring (state).
     """
-    def __init__(self, kind, timestamp_str, content_id,
-                 inputs: Iterable[InputSpec] = (),
-                 input_map=None,
-                 box_name=None,
-                 name="UNKNOWN"):
-        self.inputs: List[InputSpec] = list(inputs)
-        self.input_map = input_map if input_map else {}
-        self.content_id = content_id
-        self.box_name = box_name
-        assert isinstance(name, str)
-        self.name = name
-        self.kind = kind
-        self.timestamp_str = timestamp_str
-        self.timestamp = time_from_timestamp(timestamp_str)
-        self.state = BeadState.SUPERSEDED
+
+    content_id: str = attr.ib(kw_only=True)
+    kind: str = attr.ib(kw_only=True)
+    inputs: List[InputSpec] = attr.ib(kw_only=True, factory=list, converter=list)
+    timestamp_str: str = attr.ib(kw_only=True)
+    name: str = attr.ib(kw_only=True, default="UNKNOWN")
+    input_map: Dict[str, str] = attr.ib(kw_only=True, factory=dict, converter=none_to_empty_dict)
+    state: BeadState = attr.ib(kw_only=True, default=BeadState.SUPERSEDED)
+    box_name: Optional[str] = attr.ib(kw_only=True, default=None)
+
+    @property
+    def timestamp(self):
+        return time_from_timestamp(self.timestamp_str)
 
     @classmethod
     def from_bead(cls, bead):
@@ -46,11 +54,12 @@ class MetaBead:
         The returned bead is referenced as input from another bead,
         but we do not have the referenced bead.
         """
-        phantom = cls(
-            name=name,
-            content_id=inputspec.content_id,
-            kind=inputspec.kind,
-            timestamp_str=inputspec.timestamp_str)
+        phantom = (
+            cls(
+                name=name,
+                content_id=inputspec.content_id,
+                kind=inputspec.kind,
+                timestamp_str=inputspec.timestamp_str))
         phantom.state = BeadState.PHANTOM
         return phantom
 
