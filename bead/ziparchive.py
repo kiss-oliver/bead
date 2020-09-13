0b342cf648fd3cd986c61f9ecb5a473117a6dd29
Fekete Krisztián <e3krisztian@gmail.com>
2020-09-13 21:51:13 +0200

    Improve performance by caching open zip files

--- bead/ziparchive.py
+++ bead/ziparchive.py
@@ -1,14 +1,13 @@
 from copy import deepcopy
-import functools
 import os
 import shutil
-import zipfile
 
 from .bead import UnpackableBead
 from .exceptions import InvalidArchive
 from . import tech
 from . import layouts
 from . import meta
+from . import zipopener
 
 # technology modules
 timestamp = tech.timestamp
@@ -25,40 +24,20 @@ META_KEYS = (
 )
 
 
-def _zipfile_user(method):
-    # method is called with the zipfile opened
-    @functools.wraps(method)
-    def f(self, *args, **kwargs):
-        if self.zipfile:
-            return method(self, *args, **kwargs)
-        try:
-            with zipfile.ZipFile(self.archive_filename) as self.zipfile:
-                return method(self, *args, **kwargs)
-        except (zipfile.BadZipFile, OSError, IOError):
-            raise InvalidArchive(self.archive_filename)
-        finally:
-            self.zipfile = None
-    return f
-
-
 class ZipArchive(UnpackableBead):
 
     def __init__(self, filename, box_name=''):
         self.archive_filename = filename
         self.box_name = box_name
-        self.zipfile = None
-        # TODO: _meta should not be loaded if there is a cache file that satisfies the request,
-        #  however when loaded (any file, or property not known is requested)
-        #  it should be checked against the potentially loaded cached values to remain consistent
-        #  and prevent metadata damage.
-        #  The cached values MUST match the values in the archive meta,
-        #  with the exception of inputs, which is also a composite:
-        #    - cached input keys must be a strict subset of inputs in the meta
-        #    - if an input is cached, the input specification (value) MUST match
-        #      the one in the archive meta.
-        # Other non-meta attributes (input-map) can be cached under extra keys
         self._meta = self._load_meta()
         self._content_id = None
+
+    @property
+    def zipfile(self):
+        try:
+            return zipopener.open(self.archive_filename)
+        except (zipopener.BadZipFile, OSError, IOError):
+            raise InvalidArchive(self.archive_filename)
 
     # -
     # FIXME: Archive.is_valid is too costly for a property
@@ -80,7 +59,6 @@ class ZipArchive(UnpackableBead):
         '''
         return all(self._checks())
 
-    @_zipfile_user
     def _checks(self):
         yield self._has_well_formed_meta()
         yield self._bead_creation_time_is_in_the_past()
@@ -103,7 +81,6 @@ class ZipArchive(UnpackableBead):
         #                 2010/04/08/precision-and-accuracy-of-datetime/
         return freeze_time <= now
 
-    @_zipfile_user
     def _extra_file(self):
         data_dir_prefix = layouts.Archive.DATA + '/'
         code_dir_prefix = layouts.Archive.CODE + '/'
@@ -117,7 +94,6 @@ class ZipArchive(UnpackableBead):
                     # unexpected extra file!
                     return name
 
-    @_zipfile_user
     def _file_with_different_content_id(self):
         for name, hash in self.manifest.items():
             try:
@@ -138,7 +114,6 @@ class ZipArchive(UnpackableBead):
             self._content_id = self.calculate_content_id()
         return self._content_id
 
-    @_zipfile_user
     def calculate_content_id(self):
         # there is currently only one meta version
         # and it must match the one defined in the workspace module
@@ -165,17 +140,11 @@ class ZipArchive(UnpackableBead):
         # harm to this Archive instance
         return deepcopy(self._meta)
 
-    @_zipfile_user
     def zip_load(self, filename):
         return persistence.zip_load(self.zipfile, filename)
 
     @property
     def input_map(self):
-        # TODO: use "{archive_filename}.meta" if available by default,
-        #       fall back to metadata inside the zip file
-        # REASON: it is very expensive to update a zip file,
-        #   while appending is possible, it adds a new version of the file
-        #   with no clear guarantee, that it can be retrieved in order
         try:
             return self.zip_load(layouts.Archive.INPUT_MAP)
         except:
@@ -192,7 +161,6 @@ class ZipArchive(UnpackableBead):
         except:
             raise InvalidArchive(self.archive_filename)
 
-    @_zipfile_user
     def extract_file(self, zip_path, fs_path):
         '''
             Extract zip_path from zipfile to fs_path.
@@ -207,7 +175,6 @@ class ZipArchive(UnpackableBead):
             with open(fs_path, 'wb') as target:
                 shutil.copyfileobj(source, target)
 
-    @_zipfile_user
     def extract_dir(self, zip_dir, fs_dir):
         '''
             Extract all files from zipfile under zip_dir to fs_dir.
